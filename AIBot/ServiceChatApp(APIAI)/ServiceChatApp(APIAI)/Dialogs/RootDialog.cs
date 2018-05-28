19f2887b40faf519e6d6293b003d48d6308c3501
Rohit Sinha <rohit.sinhalala@gmail.com>
2018-05-28 13:00:50 +0530

    commit changes

--- AIBot/ServiceChatApp(APIAI)/ServiceChatApp(APIAI)/Dialogs/RootDialog.cs
+++ AIBot/ServiceChatApp(APIAI)/ServiceChatApp(APIAI)/Dialogs/RootDialog.cs
@@ -5,6 +5,7 @@ using ApiAiSDK;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Connector;
+using ServiceChatApp_APIAI_.Internals;
 
 namespace ServiceChatApp_APIAI_.Dialogs
 {
@@ -60,6 +61,16 @@ namespace ServiceChatApp_APIAI_.Dialogs
                    retry: retry_response
                    );
             }
+            
+            /*else if(action_response.Contains("CheckStatus-next"))
+            {
+                PromptDialog.Confirm(
+                  context,
+                  resume: NextCall,
+                  prompt: "Do you wish to check that out",
+                  retry: retry_response
+                  );
+            }*/
 
             /*else if(action_response.Contains("RaiseTicket-repeat"))
             {
@@ -84,6 +95,11 @@ namespace ServiceChatApp_APIAI_.Dialogs
                     retry: retry_response);
             }
 
+            else if(action_response.Contains("smalltalk.agent.can_you_help"))
+            {
+                await context.PostAsync(response + "\n" + ResponseMessage.HelpMessage);
+            }
+
             else
             {
                 MenuOption(context);
@@ -92,6 +108,15 @@ namespace ServiceChatApp_APIAI_.Dialogs
             
 
             //context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task NextCall(IDialogContext context, IAwaitable<bool> result)
+        {
+            var confirmation = await result;
+            if(confirmation.ToString() == "yes")
+            {
+                context.Call(new TicketModel(), ChildDialogcomplete);
+            }
         }
 
         /*private async void RepeatMessage(string response)
@@ -108,7 +133,7 @@ namespace ServiceChatApp_APIAI_.Dialogs
                 retry: "Please try agin, as some problem occured");
         }
 
-        private void NextCall(IDialogContext context)
+      /* private void NextCall(IDialogContext context)
         {
             PromptDialog.Confirm(
                     context,
@@ -116,99 +141,32 @@ namespace ServiceChatApp_APIAI_.Dialogs
                     prompt: "Do you wish to check that out",
                     retry: retry_response
                     );
-        }
+        }*/
 
         private async Task ResponseOption(IDialogContext context, IAwaitable<bool> result)
         {
             var confirmation = await result;
-            PromptDialog.Text(
-                context,
-                resume: DisplayTicketStatus,
-                prompt:"Please provide the ticket number for which you want to check the status",
-                retry: retry_response);
+            if (confirmation.ToString() == "yes")
+            {
+                PromptDialog.Text(
+                    context,
+                    resume: DisplayTicketStatus,
+                    prompt: "Please provide the ticket number for which you want to check the status",
+                    retry: retry_response);
+            }
+
+            else
+            {
+                await context.PostAsync("I am done for today");
+            }
         }
 
         private async Task DisplayTicketStatus(IDialogContext context, IAwaitable<string> result)
         {
             var incidentTokenNumber = await result;
 
-            string statusDetail = Logger.RetrieveIncidentServiceNow(incidentTokenNumber);
+            context.Call(new StatusDialog(incidentTokenNumber), ChildDialogcomplete);
 
-            string statusDetails = Logger.RetrieveIncidentServiceNow(incidentTokenNumber);
-
-            /**
-             * The if- else- if condition to match the state of the incident token returned by the RetrieveIncidentSerivceNow method
-             */
-
-            if (statusDetails == "1")
-            {
-                var status = "Your token is created and is under review by our team.";
-                string Notesresult = Logger.RetrieveIncidentWorkNotes(incidentTokenNumber);
-
-                var replyMessage = context.MakeMessage();
-                Attachment attachment = HeroCardDetails.GetReplyMessage(Notesresult, incidentTokenNumber, status);
-                replyMessage.Attachments = new List<Attachment> { attachment };
-                await context.PostAsync(replyMessage);
-
-            }
-
-            else if (statusDetails == "2")
-            {
-                var status = "Your ticket is in progress.";
-                string Notesresult = Logger.RetrieveIncidentWorkNotes(incidentTokenNumber);
-
-                var replyMessage = context.MakeMessage();
-                Attachment attachment = HeroCardDetails.GetReplyMessage(Notesresult, incidentTokenNumber, status);
-                replyMessage.Attachments = new List<Attachment> { attachment };
-                await context.PostAsync(replyMessage);
-
-            }
-
-            else if (statusDetails == "3")
-            {
-                await context.PostAsync("Your ticket is been kept on hold.");
-
-
-            }
-
-            else if (statusDetails == "6")
-            {
-                var status = "Your ticket is resolved.";
-
-                /**
-                 * Retrieves the details from the resolve columns of SnowLogger class if the incident token is being resolved
-                 **/
-
-                string resolveDetails = Logger.RetrieveIncidentResolveDetails(incidentTokenNumber);
-                var replyMessage = context.MakeMessage();
-                Attachment attachment = HeroCardDetails.GetReplyMessage(resolveDetails, incidentTokenNumber, status);
-                replyMessage.Attachments = new List<Attachment> { attachment };
-                await context.PostAsync(replyMessage);
-            }
-
-
-            else if (statusDetails == "7")
-            {
-                var status = "Your ticket has been closed by our team";
-
-                /**
-                 * Retrieves the close_code from the SnowLogger class if the incident token is being closed
-                 **/
-
-                string resolveDetails = Logger.RetrieveIncidentCloseDetails(incidentTokenNumber);
-                var replyMessage = context.MakeMessage();
-                Attachment attachment = HeroCardDetails.GetReplyMessage(resolveDetails + "\n" + Logger.RetrieveIncidentResolveDetails(incidentTokenNumber), incidentTokenNumber, status);
-                replyMessage.Attachments = new List<Attachment> { attachment };
-                //await context.PostAsync("Reasons for closing the ticket: " + resolveDetails);
-                await context.PostAsync(replyMessage);
-            }
-
-            else if (statusDetail == "8")
-                await context.PostAsync("Our team cancelled your ticket");
-            else
-                await context.PostAsync("Please check the ticket details. There is some mistake");
-
-            context.Done(this);
         }
 
         private async Task MenuOptionDialog(IDialogContext context, IAwaitable<string> result)
@@ -217,6 +175,7 @@ namespace ServiceChatApp_APIAI_.Dialogs
 
             string menu_response = API_AI_Logger.API_Response(res);
             string intent_response = API_AI_Logger.API_Connection_Action(res);
+
             if(intent_response.Contains("input.checkstatus"))
             {
                 StatusResponse(context, menu_response);
